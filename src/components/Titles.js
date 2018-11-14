e37caca1bac8433f5f53240161899e28488f08dd
Christina Gerard <ChristinaGerard@Christinas-MBP.attlocal.net>
2018-11-14 00:23:09 -0500

    basic functionality

---
+++ src/components/Titles.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// class Titles extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Weather Finder</h1>
+//                 <p>Find out temperarture condtions and more...</p>
+//             </div>
+//         )
+//     }
+// }
+
+const Titles = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find out temperarture condtions and more...</p>
+    </div>
+)
+
+export default Titles;
