a8a76f42b21b09d4d5898106c207ae06be724da4
Christina Gerard <ChristinaGerard@Christinas-MBP.attlocal.net>
2018-11-14 01:10:40 -0500

    styling complete

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,20 +1,9 @@
 import React from 'react';
 
-// class Titles extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Weather Finder</h1>
-//                 <p>Find out temperarture condtions and more...</p>
-//             </div>
-//         )
-//     }
-// }
-
 const Titles = () => (
     <div>
-        <h1>Weather Finder</h1>
-        <p>Find out temperarture condtions and more...</p>
+        <h1 className='title-container__title'>Weather Finder</h1>
+        <p className='title-container__subtitle'>Find out temperarture condtions and more...</p>
     </div>
 )
 
